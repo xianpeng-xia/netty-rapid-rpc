c70db7af3ed4ee9ba8f07beb16298e63bff51d61
夏先鹏 <951256916>
2022-05-22 03:51:12 +0800

    pom

---
+++ src/main/java/com/example/rpc/NettyRapidRpcApplication.java
@@ -0,0 +1,13 @@
+package com.example.rpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyRapidRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyRapidRpcApplication.class, args);
+    }
+
+}
